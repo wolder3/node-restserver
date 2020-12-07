9cf96e2c3d7135a444cf9b66c5f322e0b823116b
wolder3 <walther.cza@outlook.com>
2020-12-07 18:12:27 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,17 @@
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+//mongo db connection
+
+let urlDB;
+
+if (process.env.NODE_ENV === "dev") {
+    urlDB = "mongodb://localhost:27017/cafe";
+} else {
+    urlDB =
+        "mongodb+srv://walt:WKB5ncvLwLoarpaX@cluster0.75zrd.mongodb.net/cafe?retryWrites=true&w=majority";
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require("./config/config");
 
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const bodyParser = require("body-parser");
 
@@ -10,39 +12,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require("./routes/usuario"));
+
 app.get("/", function(req, res) {
     res.send("Hello World");
 });
 
-app.get("/usuario", function(req, res) {
-    res.json("get usuario");
-});
+console.log(process.env.URLDB);
 
-app.post("/usuario", function(req, res) {
-    let body = req.body;
+mongoose.connect(
+    process.env.URLDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    },
+    (err, res) => {
+        if (err) throw err;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario",
-        });
-    } else {
-        res.json({
-            persona: body,
-        });
+        console.log("Base de datos ONLINE");
     }
-});
+);
 
-app.put("/usuario/:id", function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id,
-    });
-});
-
-app.delete("/usuario/:id", function(req, res) {
-    res.json("delete usuario");
-});
 app.listen(process.env.PORT, () => {
     console.log("Escuchando el puerto", process.env.PORT);
 });
